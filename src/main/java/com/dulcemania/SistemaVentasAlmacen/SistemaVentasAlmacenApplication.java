162e2dbab34682e8c90102bf8fd22f9b20d54c35
Sebastian Torres <sebastorresdev@outlook.com>
2024-11-03 23:46:36 -0500

    Inicializacion del proyecto dulcemania

---
+++ src/main/java/com/dulcemania/SistemaVentasAlmacen/SistemaVentasAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.dulcemania.SistemaVentasAlmacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentasAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVentasAlmacenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dulcemania/SistemaVentasAlmacen/SistemaVentasAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulcemania.SistemaVentasAlmacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVentasAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
